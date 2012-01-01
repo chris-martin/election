c07fc0d1362fe797c9281b0362eb0e1be8925ec9
Christopher Martin <ch.martin@gmail.com>
2012-01-01 00:36:31 -0500

    create simple model for Bills

--- bills/models.py
+++ bills/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Bill(models.Model):
+  number = models.CharField(max_length = 20) # example: H.R. 3261
+  nick = models.CharField(max_length = 200) # example: SOPA
+  name = models.CharField(max_length = 200) # example: Stop Online Piracy Act
+  def __unicode__(self):
+    return self.nick
+
+class Politician(models.Model):
+  name = models.CharField(max_length = 100) # example: John Barrow
+  def __unicode__(self):
+    return self.name
+
+class Supporter(models.Model):
+  bill = models.ForeignKey(Bill)
+  politician = models.ForeignKey(Politician)
+
