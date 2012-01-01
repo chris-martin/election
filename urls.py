313cd341b7277a6e43e18c8cd63d6cf6ff2c6e52
Christopher Martin <ch.martin@gmail.com>
2012-01-01 01:02:50 -0500

    whitespace

--- urls.py
+++ urls.py
@@ -4,12 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^election/', include('election.foo.urls')),
+  # Example:
+  # (r'^election/', include('election.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+  # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+  # to INSTALLED_APPS to enable admin documentation:
+  # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    (r'^admin/', include(admin.site.urls)),
+  (r'^admin/', include(admin.site.urls)),
 )
