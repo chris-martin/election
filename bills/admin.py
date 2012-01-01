2e219387f74a543f6edc0ccae794fe3a3f43121b
Christopher Martin <ch.martin@gmail.com>
2012-01-01 00:58:34 -0500

    add Supporter inline to Bill admin

--- bills/admin.py
+++ bills/admin.py
@@ -1,6 +1,14 @@
-from election.bills.models import Bill, Politician
+from election.bills.models import Bill, Politician, Supporter
 from django.contrib import admin
 
-admin.site.register(Bill)
+class SupporterInline(admin.TabularInline):
+  model = Supporter
+  extra = 10
+
+class BillAdmin(admin.ModelAdmin):
+  inlines = [SupporterInline]
+
+admin.site.register(Bill, BillAdmin)
+
 admin.site.register(Politician)
 
